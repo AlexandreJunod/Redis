d137ae09c9b74d8d0920dd77f456d27c68ea477c
Dardan Iljazi <sleepersdardan@gmail.com>
2019-01-16 22:41:46 +0100

    Add overriden function into Login Controller

--- RedisApp/app/Http/Controllers/Auth/LoginController.php
+++ RedisApp/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -35,5 +38,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->only(["email","password"]);
+
+        if (Redis::hget(sha1($credentials["email"] . $credentials["password"]), "email") === $credentials["email"]) {
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+        return $this->sendFailedLoginResponse($request);
     }
 }
